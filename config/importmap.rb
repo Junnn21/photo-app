493ad59e6c8580fdd3ccc90de813c96b8aab00aa
EC2 Default User <cjun580@gmail.com>
2022-11-01 05:14:09 +0000

    Fix Prod

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,9 +1,6 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
-pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "jquery", to: "https://code.jquery.com/jquery-3.6.1.min.js", preload: true
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.2.1/dist/js/bootstrap.esm.js", preload: true
